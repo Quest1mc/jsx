8c73ed60e5da4017b0bbe76d656590b99e8bf8b4
Stefan Quest <stefan.quest@gmail.com>
2019-05-14 14:01:04 +0200

    completed jsx lesson with refrenced excercise

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 // import the react and ReactDOM libraries 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+// function getbuttonText () {
+//     return 'Click Me!'
+// }
 // Create a react component
 const App = () => {
-    return <div> Hi there! </div>;
+    const buttonText = 'Click Me!'
+    // note to self to use a js variable in jsx include it in {}
+    // or you can call a function inside the {} it works as well 
+    return (
+    <div> 
+        <label className="label" htmlFor = "name">
+        Enter name:
+        </label>
+        <input id= "name" type= "text" />
+        {/* before  <button style = "background-color: blue; color: white;">Submit</button>  */}
+       {/* after  */} <button style ={{backgroundColor:'blue', color:'white'}}>
+      {buttonText} 
+       </button> 
+        
+    </div>
+    )
 };
 
 // Take the React Component and show it on the screen 
